79836732eea03889b69b5e69aa956031b33b4aaf
DHLoudounStairs <david.hamilton@lsi.local>
2025-06-05 10:52:47 -0600

    categories

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -15,6 +15,7 @@
                 <flux:navlist.group :heading="__('Platform')" class="grid">
                     <flux:navlist.item icon="home" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>{{ __('Dashboard') }}</flux:navlist.item>
                     <flux:navlist.item icon="bolt" :href="route('trainings.create')" :current="request()->routeIs('trainings.create')" wire:navigate>{{ __('Training') }}</flux:navlist.item>
+                     <flux:navlist.item icon="bolt" :href="route('categories.index')" :current="request()->routeIs('categories.index')" wire:navigate>{{ __('Categories') }}</flux:navlist.item>
                 </flux:navlist.group>
             </flux:navlist>
 
